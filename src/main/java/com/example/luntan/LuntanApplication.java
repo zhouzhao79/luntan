22903a941edf370e748d76297d6759c27d7ef1ce
zhouzhao <798001923@qq.com>
2020-03-05 23:52:58 +0800

    提交

---
+++ src/main/java/com/example/luntan/LuntanApplication.java
@@ -0,0 +1,13 @@
+package com.example.luntan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuntanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuntanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luntan/LuntanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luntan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuntanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
