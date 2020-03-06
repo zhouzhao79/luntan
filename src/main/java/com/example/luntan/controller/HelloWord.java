0c954a0a1b702e89065c3957925c6cd2d732e87c
zhouzhao <798001923@qq.com>
2020-03-06 09:19:58 +0800

    20200306编辑提交

---
+++ src/main/java/com/example/luntan/controller/HelloWord.java
@@ -0,0 +1,15 @@
+package com.example.luntan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWord {
+    @GetMapping("/")
+    public String Hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
