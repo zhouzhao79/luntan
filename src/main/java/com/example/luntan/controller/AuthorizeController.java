de2388737f7b6ed58b30342bf1dc89a1a8a6fec1
zhouzhao <798001923@qq.com>
2020-03-07 22:53:25 +0800

    获取cookie

--- src/main/java/com/example/luntan/controller/AuthorizeController.java
+++ src/main/java/com/example/luntan/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -38,7 +40,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name="state")String state,
-                           HttpServletRequest request){
+                 //          HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -50,15 +53,19 @@ public class AuthorizeController {
         if (gitHubUser !=null){
             //存进数据库
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(gitHubUser.getName());
             user.setAccountId(String.valueOf(gitHubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-
+            //账户长期在线设置
+            //写入cookie
+            response.addCookie(new Cookie("token",token));
             //登录成功 写cookie 和 session
-            request.getSession().setAttribute("user",gitHubUser);
+
+            //request.getSession().setAttribute("user",gitHubUser);  向数据库存储就是这一步的意思
             return "redirect:/";
 
         }else
--- src/main/java/com/example/luntan/controller/IndexController.java
+++ src/main/java/com/example/luntan/controller/IndexController.java
@@ -1,16 +1,40 @@
 package com.example.luntan.controller;
 
+import com.example.luntan.mapper.UserMapper;
+import com.example.luntan.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;//这样是有问题的，需要service层
+
     @GetMapping("/")
-    public String index(){
-//        model.addAttribute("name",name);
-//        @RequestParam(name = "name")String name, Model model
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+       // if (cookies.length==0) {
+            for (Cookie cookie : cookies
+            ) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+
+            }
+     //   }
         return "index";
     }
+
+
 }
