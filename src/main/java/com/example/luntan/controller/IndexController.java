898599055017ff4e13465e88806becae4a8b0803
zhouzhao <798001923@qq.com>
2020-03-09 13:38:59 +0800

    20200309 qidong tijiao

--- src/main/java/com/example/luntan/controller/IndexController.java
+++ src/main/java/com/example/luntan/controller/IndexController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class IndexController {
@@ -17,9 +19,11 @@ public class IndexController {
     private UserMapper userMapper;//这样是有问题的，需要service层
 
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request, HttpServletResponse response){
+        String cookie1=request.getHeader("cookie");
+        response.addCookie(new Cookie("suijiID", UUID.randomUUID().toString()));
         Cookie[] cookies = request.getCookies();
-       // if (cookies.length==0) {
+        if (cookies!=null) {
             for (Cookie cookie : cookies
             ) {
                 if (cookie.getName().equals("token")) {
@@ -32,7 +36,7 @@ public class IndexController {
                 }
 
             }
-     //   }
+       }
         return "index";
     }
 
